8787e96b4a09482e0964d6f62b658383db849f0b
Harsimran Singh <harsimrans@simsaw.com>
2018-10-22 15:11:08 +0530

    feat: fetch speak cases API

--- controllers/dakk/fetch.js
+++ controllers/dakk/fetch.js
@@ -2,10 +2,12 @@ import joi from 'joi';
 import Boom from 'boom';
 import db from '../../db';
 
-const { Dakk, Files } = db.models;
+const { Dakk, Files, DakkUser, User } = db.models;
 
 Dakk.hasMany(Files, {foreignKey: 'id'});
 Files.belongsTo(Dakk, { foreignKey: 'dakkId' });
+Dakk.hasMany(DakkUser, {foreignKey: 'dakkId'});
+DakkUser.belongsTo(User, {foreignKey: 'userId'});
 
 module.exports = {
   auth: 'jwt',
@@ -26,17 +28,49 @@ module.exports = {
       offset: joi
         .number()
         .default(10)
-        .description('Offset number')
+        .description('Offset number'),
+
+      speakUp: joi
+        .string()
+        .valid('all', 'pending')
+        .optional()
+        .description('Speak Up filter')
     }
   },
 
   handler: async (request, h) => {
-    const {page, offset} = request.query;
+    const { page, offset, speakUp } = request.query;
+    const condition = {};
 
     try {
+      if (speakUp) {
+        if (speakUp === 'pending') {
+          condition.speakOn = 1;
+        }
+
+        const dakks = await Dakk
+        .findAll({
+          attributes: ['name', 'updatedAt', 'speakOn', 'id'],
+          where: condition,
+          include: [
+            { model: DakkUser, attributes: ['userId'], include: [{ model: User, attributes: ['userName'] }] }
+          ],
+          offset: page === 0 ? 0 : (page * offset),
+          limit: offset,
+        });
+
+        const count = await Dakk.count({ where: condition });
+
+        return h.response({
+          rows: dakks,
+          count
+        });
+      }
+
       const dakks = await Dakk
       .findAll({
         attributes: ['name', 'status', 'id'],
+        condition,
         include: [
           Files
         ],
